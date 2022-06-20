d6492a398a8582b186ddaf898aea6d15acf59695
lyndalili <lyndahaddad50@gmail.com>
2022-06-20 14:25:17 +0200

    first version

---
+++ index.js
@@ -0,0 +1,43 @@
+//console.log("Hello World !")
+
+//let sentence = "my name is lh"
+//console.log(sentence)
+
+//let name = "lynda"
+//console.log(`my name is ${name}`)
+
+//let sentence = "I'm a new text"
+//console.log(sentence)
+
+//let food = "croissant is meh"
+//food = food.replace("meh", "so good")
+//console.log(food)
+
+//let basic = "This is Cool"
+//let basicUp = "THIS IS COOL"
+//let basicDown = "this is cool"
+//console.log(basic + basicUp + basicDown)
+
+//let word = "banana"
+//let letters = ['w', 'o', 'r', 'd']
+//console.log(letters)
+
+//const age = 28
+//const template = "I'm " + age + " years old"
+//console.log(template)
+
+
+//Bonus
+
+
+//let voiture = {
+//marque: "audi",
+//couleur: "red",
+//nombredeporte: 5,
+//option: ["toit ouvrant", "clim", "camera de recule"],
+//}
+//console.log(voiture.marque)
+//console.log(voiture.couleur)
+
+
+//console.dir (document);
